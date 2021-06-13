7caa07cb16a8c425fd6a4a146a4f65ff1767e7ec
Dennis Mwaniki <damos5776@gmail.com>
2021-06-13 20:02:04 +0300

    female names and days of the week

--- js/script.js
+++ js/script.js
@@ -5,3 +5,6 @@ alert('Welcome!Get to know your Akan name')
 
 var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
