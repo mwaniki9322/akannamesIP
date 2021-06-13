28d5b5075f9fa7a47a8f4139643f3bd105892e44
Dennis Mwaniki <damos5776@gmail.com>
2021-06-13 20:09:23 +0300

    set date of birth input as the current date

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function result(){
+    var dateofbirthinput=document.getElementById('datepicker').value;
+
+    //input set to current date
+    var date=new Date(dateofbirthinput);
+
+    //get day of the week
+    var weekday=date.getDay();
+
+
+}
