bb6d1fd9c71e4facd82d5a34b2403b599f71ad94
Dennis Mwaniki <damos5776@gmail.com>
2021-06-13 20:00:56 +0300

    male names

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ var welcome=function(welcoming){
     alert(welcoming)
 }
 alert('Welcome!Get to know your Akan name')
+
+var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  
